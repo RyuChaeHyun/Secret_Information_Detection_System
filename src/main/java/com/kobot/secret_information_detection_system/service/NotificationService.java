4a9ffde75578334e08a7e9e8481054c253495219
SeokCheol <lsc991123@gmail.com>
2024-09-25 15:47:26 +0900

    feat : Secret Information Detection System

--- src/main/java/com/kobot/secret_information_detection_system/service/NotificationService.java
+++ src/main/java/com/kobot/secret_information_detection_system/service/NotificationService.java
@@ -1,18 +1,19 @@
 package com.kobot.secret_information_detection_system.service;
 
 import java.io.IOException;
+import org.springframework.beans.factory.annotation.Value;
 import lombok.RequiredArgsConstructor;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class NotificationService {
 
+  @Value("${slack.webhook}")
+  private String webhookUrl;
+
   // Slack 알림 전송
   public void sendSlackNotification(String message) throws IOException {
-    String webhookUrl = "https://hooks.slack.com/services/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";
     String payload = "{\"text\":\"" + message + "\"}";
 
     ProcessBuilder processBuilder = new ProcessBuilder("curl", "-X", "POST", "-H", "Content-type: application/json", "--data", payload, webhookUrl);
